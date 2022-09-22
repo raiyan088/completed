837ab98ee7e09ec2d472e8cf250f30728d94c4c0
raiyan088 <78313577+raiyan088@users.noreply.github.com>
2022-09-22 21:25:41 +0600

    Add files via upload

---
+++ server.js
@@ -0,0 +1,37 @@
+const SocketServer = require('websocket').server
+const express = require('express')
+const https = require('https')
+const http = require('http')
+
+var app = express()
+
+const server = http.createServer(app)
+
+server.listen(process.env.PORT || 3000, ()=>{
+    console.log("Listening on port 3000...")
+})
+
+let wsServer = new SocketServer({httpServer:server})
+
+wsServer.on('request', (req) => {
+    let connection = req.accept()
+    console.log('Request Accept')
+    connection.on('message', (message) => {
+        try {
+            if(message.type === 'utf8') {
+                console.log(message)
+                connection.send(message.utf8Data)
+            }
+        } catch (e) {}
+    })
+    
+    connection.on('close', function() {
+    })
+})
+
+app.get('/', async function(req, res) {
+    res.writeHeader(200, {"Content-Type": "text/html"})
+    res.write('Success')
+    res.end()
+})
+
