2eb615abaf3e5bd53b4a027209dcf47fca522efb
raiyan088 <78313577+raiyan088@users.noreply.github.com>
2022-09-23 03:54:20 +0600

    Update server.js

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ wsServer.on('request', (req) => {
 
 app.get('/', async function(req, res) {
     res.writeHeader(200, {"Content-Type": "text/html"})
-    res.write(os.cpus().length+'\n'start+'\n'+new Date().toString())
+    res.write(os.cpus().length+'\n'+start+'\n'+new Date().toString())
     res.end()
 })
 
